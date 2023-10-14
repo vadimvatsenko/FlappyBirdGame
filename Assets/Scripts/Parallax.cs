15bb64df843cc3b27a60999f854afb672d93cd43
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2023-10-14 10:32:52 +0300

    Start Project and add bird player whith background

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+  private MeshRenderer meshRenderer; // это компонент который является типом для отображения 3D обьектов, который позволяет отрисовать модели, текстуры и материалы
+    public float animationSpeed = 0.1f;
+
+    private void Awake()
+    {
+        animationSpeed = 0.1f;
+        meshRenderer = GetComponent<MeshRenderer>(); // Значение, полученное с помощью GetComponent,
+                                                     // присваивается переменной meshRenderer, которая была объявлена ранее.
+                                                     // Это позволяет вашему скрипту сохранить ссылку на компонент MeshRenderer,
+                                                     // чтобы в дальнейшем использовать его для управления отображением 3D-графики этого объекта.
+    }
+
+    private void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2 (animationSpeed * Time.deltaTime, 0); //  применяет смещение текстуры объекта вдоль горизонтальной оси (X) - animationSpeed * Time.deltaTime
+                                                                                                     // (Y) - 0
+                                                                                                     //  на основе значения animationSpeed, что может использоваться
+                                                                                                     //  для создания анимации движения текстуры на поверхности объекта.
+    }
+
+}
+
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer spriteRender;
+    public Sprite[] sprites; // создаем массив для хранения всех спрайтов птички
+    private int spriteIndex; // переменная для хранения индекса спрайта
+
+    private Vector3 direction; // 
+
+    public float gravity = -9.81f; // устанавливаем гравитацию для персонажа, это значение настоящей гравитации в жизни
+
+    public float strength = 5f; // сила взмаха птицы
+
+    private void Awake()
+    {
+        spriteRender = GetComponent<SpriteRenderer>(); // получаем ссылку на компонент SpriteRenderer, он ищет его в нашем компоненте, в данном случае Player
+    }
+
+    private void Start() // будет запущен при первом запуске игры после выполнения метода Awake()
+    {
+        //Application.targetFrameRate = 60;
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f); // повторный вызов метода для переключения спрайтов через 15мс и каждые 15мс
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) 
+        {
+            direction = Vector3.up * strength; // увеличение по оси y
+        }
+
+        if(Input.touchCount > 0) // если нажатий по экрану больше чем 0
+        {
+            Touch touch = Input.GetTouch(0); // означает, что вы получаете информацию о событии касания (touch event) на экране и сохраняете эту информацию в переменной touch.
+
+            if (touch.phase == TouchPhase.Began) // есди кассание началось, то выполни действие ниже
+            {
+                direction = Vector3.up * strength;
+            }
+        }
+
+        direction.y += gravity * Time.deltaTime; // гравитация умноженая на время, в данном случае будет отрицательное значение 
+
+        transform.position += direction * Time.deltaTime; // теперь позиция птици будет сдвигатся вниз под силой гравитации
+    }
+
+    private void AnimateSprite() // функция которая увеличивает значение индекса спрайта на +1
+    {
+        spriteIndex++;
+
+        if(spriteIndex >= sprites.Length) // если индекс спрайта больше или равно длинне спрайта, вернуть индекс значения спрайта на 0. Это сделано для того, чтобы заново запустить анимацию
+        {
+            spriteIndex = 0;
+        }
+
+        spriteRender.sprite = sprites[spriteIndex]; // переключает спрайты по очереди
+
+    }
+
+}
